4b2b52cdfb88089123bed2baf7c0db9c92ec6b69
AndreiaLima19 <amloliveira@gmail.com>
2021-09-29 17:46:30 -0300

    Descrição Pokemons

--- index.js
+++ index.js
@@ -15,20 +15,51 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
     //const conteudos = ["Express","Dependências","Projeto 2"];
     const conteudos = [
-        {nome: 'teste nome',
-        tipo: 'teste tipo',
-        descricao: 'teste descricao'
+        {numero: '010',
+        nome: 'Caterpie ',
+        tipo: 'Bug',
+        imagem: '/img/caterpie.png',
+        descricao: 'For protection, it releases a horrible stench from the antenna on its head to drive away enemies',
+        altura: '0.3 m',
+        peso: '2.9 kg',
+        categoria: 'Worm',
+        habilidade: 'Shield Dust'
         },
-        {nome: 'teste nome2',
-        tipo: 'teste tipo2',
-        descricao: 'teste descricao2'
+        {numero: '011',
+        nome: 'Metapod',
+        tipo: 'Bug',
+        imagem: '/img/metapod.png',
+        descricao: 'It is waiting for the moment to evolve. At this stage, it can only harden, so it remains motionless to avoid attack',
+        altura: '0.7 m',
+        peso: '9.9 kg',
+        categoria: 'CategoryCocoon',
+        habilidade: 'Shed Skin'
         },
-        {nome: 'teste nome3',
-        tipo: 'teste tipo3',
-        descricao: 'teste descricao3'
-        },
+        {numero: '012',
+        nome: 'Butterfree',
+        tipo: 'Bug, Flying',
+        imagem: '/img/butterfree.png',
+        descricao: 'In battle, it flaps its wings at great speed to release highly toxic dust into the air',
+        altura: '1.1 m',
+        peso: '32.0 kg',
+        categoria: 'Butterfly',
+        habilidade: 'Compound Eyes'
+        }
+
+        // {nome: 'teste nome',
+        // tipo: 'teste tipo',
+        // descricao: 'teste descricao'
+        // },
+        // {nome: 'teste nome2',
+        // tipo: 'teste tipo2',
+        // descricao: 'teste descricao2'
+        // },
+        // {nome: 'teste nome3',
+        // tipo: 'teste tipo3',
+        // descricao: 'teste descricao3'
+        // },
     ];
-    res.render("index", {titulo: "Codelab Aula 07", lista:conteudos}); // Nome do arquivo, o EJS já busca dentro da pasta views.
+    res.render("index", {titulo: "Pokédex", lista:conteudos}); // Nome do arquivo, o EJS já busca dentro da pasta views.
 });
 
 app.get("/cadastro", (req, res) => {
